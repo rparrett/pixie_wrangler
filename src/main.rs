fe881f7c0c4295f57097e7d6018439c6da4d59e0
Rob Parrett <robparrett@gmail.com>
2021-07-07 07:29:53 -0700

    Add probably-unneeded point-segment intersection code

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,33 @@ enum SegmentCollision {
     None,
 }
 
+fn point_segment_collision(p: Vec2, l1: Vec2, l2: Vec2) -> SegmentCollision {
+    if p == l1 || p == l2 {
+        return SegmentCollision::Connecting;
+    }
+
+    let d1 = l2 - l1;
+    let d2 = p - l1;
+
+    let cross = d1.perp_dot(d2);
+
+    if cross.abs() > std::f32::EPSILON {
+        return SegmentCollision::None;
+    }
+
+    let dot = d1.dot(d2);
+
+    if dot < 0.0 {
+        return SegmentCollision::None;
+    }
+
+    if dot > l1.distance_squared(l2) {
+        return SegmentCollision::None;
+    }
+
+    SegmentCollision::Touching
+}
+
 // for reference, this is helpful
 // https://github.com/pgkelley4/line-segments-intersect/blob/master/js/line-segments-intersect.js
 // but we're differing pretty wildly in how we choose to deal with collinearities, and
@@ -343,6 +370,53 @@ fn setup(mut commands: Commands) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn test_point_segment_collision() {
+        // -.-
+        assert!(matches!(
+            point_segment_collision(
+                Vec2::new(0.0, 0.0),
+                Vec2::new(-1.0, 0.0),
+                Vec2::new(1.0, 0.0)
+            ),
+            SegmentCollision::Touching
+        ));
+        assert!(matches!(
+            point_segment_collision(
+                Vec2::new(0.0, 0.0),
+                Vec2::new(0.0, -1.0),
+                Vec2::new(0.0, 1.0)
+            ),
+            SegmentCollision::Touching
+        ));
+        // .--
+        assert!(matches!(
+            point_segment_collision(
+                Vec2::new(0.0, 0.0),
+                Vec2::new(0.0, 0.0),
+                Vec2::new(1.0, 2.0)
+            ),
+            SegmentCollision::Connecting
+        ));
+        // --.
+        assert!(matches!(
+            point_segment_collision(
+                Vec2::new(1.0, 2.0),
+                Vec2::new(0.0, 0.0),
+                Vec2::new(1.0, 2.0)
+            ),
+            SegmentCollision::Connecting
+        ));
+        assert!(matches!(
+            point_segment_collision(
+                Vec2::new(1.0, 1.0),
+                Vec2::new(0.0, 0.0),
+                Vec2::new(1.0, 0.0)
+            ),
+            SegmentCollision::None
+        ));
+    }
+
     #[test]
     fn test_segment_collision() {
         // collinear non-overlapping x axis
